6fede00fcfe8edad3bf6edd298325b18d5d22347
Rachel Yao <42477010+rachelya0@users.noreply.github.com>
2022-08-05 21:38:26 -0500

    working pin with replaceable image

--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const helpers = require("./helpers");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/temp');
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: helpers.imageFilter
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + "/public"));
+
+app.post("/upload", upload.single("uploaded-pic"), function(req, res) {
+  const oldPath = req.file.path;
+  const newPath = path.join(__dirname, "./uploads/image.png");
+
+  fs.rename(oldPath, newPath, function(error) {
+    if (error) {
+      return handleError(error, res);
+    }
+  });
+  res.redirect("/");
+  //res.sendFile(`${__dirname}/uploads/${req.file.filename}`)
+});
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 })
 
-app.get("/contact", function(req, res) {
-  res.send("Contact me at: rachelyao.ling@gmail.com");
-})
+app.get("/image.png", function(req, res) {
+  res.sendFile(path.join(__dirname, "./uploads/image.png"));
+});
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
